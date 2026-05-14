fb876e2dc5d7198862d7279552f92a9b1e2630f4
Udayan Banerji <udayanbanerji@Udayans-MacBook-Pro.local>
2026-05-14 16:33:49 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    extra.apply {
+        set("compose_version", "1.4.0")
+        set("agp_version", "8.13.0")
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "9.2.1" apply false
+    id("com.android.library") version "9.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+}
